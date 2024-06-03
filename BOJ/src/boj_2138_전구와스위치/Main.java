e650a911911a6c9abb8ac40796e2014c9b4e7f86
49turtle <turtlebsj49@gmail.com>
2024-06-03 14:54:57 +0900

    Add BOJ 2138

---
+++ BOJ/src/boj_2138_전구와스위치/Main.java
@@ -0,0 +1,47 @@
+package boj_2138_전구와스위치;
+
+import java.util.Scanner;
+
+public class Main {
+
+	static int N;
+	
+	public static void main(String[] args) {
+		
+		Scanner sc =  new Scanner(System.in);
+		
+		N = sc.nextInt();
+		
+		int[] before = new int[N];
+		int[] after = new int[N];
+		
+		String temp = sc.next();
+		
+		for (int i=0; i<N; i++) {	
+			if (temp.charAt(i) == 0) {
+				before[i] = 0;
+				continue;
+			}
+			before[i] = 1;
+		}
+		
+		
+		temp = sc.next();
+		
+		for (int i=0; i<N; i++) {
+			if (temp.charAt(i) == 0) {
+				after[i] = 0;
+				continue;
+			}
+			after[i] = 1;
+		}
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+}
