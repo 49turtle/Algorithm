9b1d989c902df1b7c0463d82036f992229264611
49turtle <turtlebsj49@gmail.com>
2024-06-13 18:32:34 +0900

    Add BOJ LCA

--- BOJ/src/boj_11437_LCA/Main_성진.java
+++ BOJ/src/boj_11437_LCA/Main_성진.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Main_성진 {
 
     static int[] depth;
     static int[] p;
--- BOJ/src/boj_11437_LCA/Main_성진_실패.java
+++ BOJ/src/boj_11437_LCA/Main_성진_실패.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main_실패 {
+public class Main_성진_실패 {
 
     static int[][] p;
 
