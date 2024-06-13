b40a66c6ef5774453e16fc4d4fac3e5eb19b67f6
49turtle <turtlebsj49@gmail.com>
2024-06-13 18:23:35 +0900

    Add BOJ LCA

--- BOJ/src/boj_11437_LCA/Main.java
+++ BOJ/src/boj_11437_LCA/Main.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 
 public class Main {
 
-    static int[][] p;
+    static int[] depth;
+    static int[] p;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -21,23 +22,24 @@ public class Main {
             edges[i][1] = sc.nextInt();
         }
 
-        // 인접 리스트 생성
-        List<List<Integer>> adjList = new ArrayList<>();
+        List<Integer>[] adjList = new ArrayList[n + 1];
         for (int i = 0; i <= n; i++) {
-            adjList.add(new ArrayList<>());
+            adjList[i] = new ArrayList<>();
         }
-
+        
         for (int[] edge : edges) {
-            adjList.get(edge[0]).add(edge[1]);
-            adjList.get(edge[1]).add(edge[0]);
+            adjList[edge[0]].add(edge[1]);
+            adjList[edge[1]].add(edge[0]);
         }
 
-        int[][] ancestors = new int[n + 1][];
+        depth = new int[n + 1];
+        p = new int[n + 1];
+        
         boolean[] visited = new boolean[n + 1];
 
-        // 조상 노드 계산
-        // 1번 노드부터 메소드 시작
-        getAncestors(adjList, ancestors, 1, new ArrayList<>(), visited);
+        // 깊이와 부모 노드 계산
+        // depth는 0부터 시작, 루트는 1번 노드부터 시작
+        dfs(adjList, 1, 0, visited);
 
         int M = sc.nextInt();
 
@@ -45,48 +47,53 @@ public class Main {
             int node1 = sc.nextInt();
             int node2 = sc.nextInt();
 
-            System.out.println(findLCA(ancestors, node1, node2));
+            System.out.println(findLCA(node1, node2));
         }
     }
 
-    static void getAncestors(List<List<Integer>> adjList, int[][] ancestors, int current, List<Integer> currentAncestors, boolean[] visited) {
-        // 방문 체크
-        visited[current] = true;
+    // 기존에 실패했던 getAncestors라는 메소드는 dfs처럼 재귀적으로 동작하면서 조상을 담아두지만,
+    // 따로 조상리스트를 메모리에 담아두지 않고, dfs 재귀로 트리를 순회하면서 depth를 저장한다면
+    // 전체의 조상 리스트를 담아두는 것보다 메모리 소모가 덜할 것
+    static void dfs(List<Integer>[] adjList, int curr, int d, boolean[] visited) {
+        visited[curr] = true;
+        // 각 노드의 depth를 저장한다.
+        depth[curr] = d;
 
-        // 현재까지 저장한 조상 리스트 임시 리스트로 저장
-        List<Integer> tempList = new ArrayList<>(currentAncestors);
-        tempList.add(current);
-
-        // 조상 리스트를 배열로 변환하여 저장
-        int[] ancestorArr = new int[tempList.size()];
-        for (int i = 0; i < tempList.size(); i++) {
-            ancestorArr[i] = tempList.get(i);
-        }
-        ancestors[current] = ancestorArr;
-
-        // 만약 인접 리스트에 내가 아직 방문하지 않은 노드가 존재한다면?
-        // getAncestors 메소드 호출
-        for (int n : adjList.get(current)) {
-            if (!visited[n]) {
-                getAncestors(adjList, ancestors, n, tempList, visited);
+        // 인접리스트를 돌면서 깊이 우선 탐색할 것
+        for (int node : adjList[curr]) {
+            if (!visited[node]) {
+                p[node] = curr;	// 도형이 형 코드처럼 부모 노드를 저장하기
+                dfs(adjList, node, d + 1, visited);
             }
         }
     }
 
-    static int findLCA(int[][] ancestors, int node1, int node2) {
-        int len1 = ancestors[node1].length;
-        int len2 = ancestors[node2].length;
-        int minLen = Math.min(len1, len2);
-
-        // 조상 리스트의 처음부터 비교하며 LCA 찾기
-        for (int i = 0; i < minLen; i++) {
-            if (ancestors[node1][i] != ancestors[node2][i]) {
-                // 이전 조상이 가장 가까운 공통 조상
-                return ancestors[node1][i - 1];
-            }
+    // LCA를 찾기 위해서 우선 확인하고자 하는 노드의 depth를 확인한다.
+    // depth가 작은 노드를 기준으로 depth를 통일하고, 그 바로 위에 존재하는 부모 노드를 불러오면 된다.
+    static int findLCA(int node1, int node2) {
+        // 두 노드의 깊이를 맞추는 로직
+    	// node1, node2 중 한 쪽의 depth가 다르다면 해당 노드의 부모 노드로 이동하고, 이를 반복
+    	if (depth[node1] != depth[node2]) {
+    		if (depth[node1] > depth[node2]) {
+        		while(depth[node1] > depth[node2]) {
+        			node1 = p[node1];
+        		}
+        	}
+        	else {
+        		while (depth[node2] > depth[node1]) {
+                    node2 = p[node2];
+                }	
+        	}
+    	}
+    	
+        // 두 노드의 depth가 같아졌다면?
+    	// 이번엔 두 노드의 번호가 같아질 때까지 부모로 이동한다.
+        // 즉, 공통 조상 LCA를 찾는다.
+        while (node1 != node2) {
+            node1 = p[node1];
+            node2 = p[node2];
         }
 
-        // 여기까지 왔으면 전체 조상 리스트가 같으므로 마지막 원소가 LCA!
-        return ancestors[node1][minLen - 1];
+        return node1;
     }
 }
