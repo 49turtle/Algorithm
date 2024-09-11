48bef43bc0920e26ef58582b156272d2ef02b709
49turtle <turtlebsj49@gmail.com>
2024-09-11 21:34:23 +0900

    Add BOJ 13565

--- BOJ/src/boj_13565_침투/Main.java
+++ BOJ/src/boj_13565_침투/Main.java
@@ -20,8 +20,9 @@ public class Main {
 		int[][] map = new int[M][N];
 
 		for (int r = 0; r < M; r++) {
+			// 문자열로 입력받고
 			String str = sc.next();
-
+			// charAt 메서드로 2차원 배열 map에 int형으로 저장하기
 			for (int c = 0; c < N; c++) {
 				int num = str.charAt(c);
 				if (num == '0') {
@@ -31,35 +32,30 @@ public class Main {
 				}
 			}
 		}
-//		printMap(map);
 
+		// BFS
 		Queue<Integer> queue = new ArrayDeque<>();
-
 		for (int i = 0; i < N; i++) {
 			if (map[0][i] == 0) {
 				int num = 0 + i;
 				queue.add(num);
 			}
 		}
-
 		while (!queue.isEmpty()) {
-
 			int num = queue.poll();
-
 			int nr = num / N;
 			int nc = num % N;
-
+			// 검사한 곳은 2로 변환
 			map[nr][nc] = 2;
-
 			for (int d = 0; d < 4; d++) {
 
 				int r = nr + dr[d];
 				int c = nc + dc[d];
 
 				if (r >= 0 && r < M && c >= 0 && c < N && map[r][c] == 0) {
+					// 마지막 줄에 도달한 경우(침투된 경우)
 					if (r == M - 1) {
-//						printMap(map);
-						System.out.println("YES");
+						System.out.println("YES");	// YES를 출력하고, 종료
 						return;
 					}
 					map[r][c] = 2;
@@ -70,19 +66,11 @@ public class Main {
 
 		}
 
-//		printMap(map);
-
+		// 전부 검사했지만, YES를 출력하지 못한 경우 NO를 출력
 		System.out.println("NO");
 
 	}
 
-	static void printMap(int[][] map) {
-		for (int i = 0; i < map.length; i++) {
-			System.out.println();
-			for (int j = 0; j < map[i].length; j++) {
-				System.out.print(map[i][j] + " ");
-			}
-		}
-	}
+
 
 }
